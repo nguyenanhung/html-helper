a67463bb860120a9bb9d8877361532b20ed83574
nguyenanhung <info@nguyenanhung.com>
2021-09-22 14:10:00 +0700

    Update HTML Helper: Add form helper

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -278,8 +278,33 @@ if (!function_exists('clean_title')) {
     function clean_title($str = '')
     {
         $str = strip_tags($str);
+        $str = htmlspecialchars($str, ENT_QUOTES);
+        if (function_exists('html_escape')) {
+            $str = html_escape($str);
+        }
+        if (function_exists('escapeHtmlAttr')) {
+            $str = escapeHtmlAttr($str);
+        }
 
         return trim($str);
+    }
+}
+if (!function_exists('get_pagination_number')) {
+    /**
+     * Function get_pagination_number
+     *
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 2019-03-25 14:03
+     *
+     * @param $str
+     *
+     * @return int
+     */
+    function get_pagination_number($str)
+    {
+        $str = str_replace('trang-', '', $str);
+
+        return (int) $str;
     }
 }
 if (!function_exists('seo_meta_tag_equiv')) {
@@ -296,5 +321,249 @@ if (!function_exists('seo_meta_tag_equiv')) {
     function seo_meta_tag_equiv($data = [])
     {
         return (new nguyenanhung\Libraries\HTML\Common())->meta($data);
+    }
+}
+if (!function_exists('form_label')) {
+    /**
+     * Creates a label for an input
+     *
+     * @param string $text       The label text
+     * @param string $fieldName  Name of the input element
+     * @param array  $attributes HTML attributes
+     *
+     * @return string
+     */
+    function form_label($text, $fieldName = null, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::label($text, $fieldName, $attributes);
+    }
+}
+if (!function_exists('form_text')) {
+    /**
+     * Creates a text field
+     *
+     * @param string $name
+     * @param string $value
+     * @param array  $attributes HTML attributes
+     *
+     * @return string
+     */
+    function form_text($name, $value = null, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::text($name, $value, $attributes);
+    }
+}
+if (!function_exists('form_password')) {
+    /**
+     * Creates a password input field
+     *
+     *
+     *
+     * @param string $name
+     * @param string $value
+     * @param array  $attributes HTML attributes
+     *
+     * @return string
+     */
+    function form_password($name, $value = null, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::password($name, $value, $attributes);
+    }
+}
+if (!function_exists('form_hidden')) {
+    /**
+     * Creates a hidden input field
+     *
+     *
+     *
+     * @param string $name
+     * @param string $value
+     * @param array  $attributes
+     *
+     * @return string
+     */
+    function form_hidden($name, $value, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::hidden($name, $value, $attributes);
+    }
+}
+if (!function_exists('form_textArea')) {
+    /**
+     * Creates a textarea
+     *
+     * @param string $name
+     * @param string $text
+     * @param array  $attributes HTML attributes
+     *
+     * @return string
+     */
+    function form_textArea($name, $text = null, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::textArea($name, $text, $attributes);
+    }
+}
+if (!function_exists('form_checkBox')) {
+    /**
+     * Creates a check box.
+     * By default creates a hidden field with the value of 0, so that the field is present in $_POST even when not checked
+     *
+     * @param string      $name
+     * @param bool        $checked
+     * @param mixed       $value           Checked value
+     * @param array       $attributes      HTML attributes
+     * @param bool|string $withHiddenField Pass false to omit the hidden field or "array" to return both parts as an array
+     *
+     * @return string|array
+     */
+    function form_checkBox($name, $checked = false, $value = 1, array $attributes = array(), $withHiddenField = true)
+    {
+        return nguyenanhung\Libraries\HTML\Form::checkBox($name, $checked, $value, $attributes, $withHiddenField);
+    }
+}
+if (!function_exists('form_collectionCheckBoxes')) {
+    /**
+     * Creates multiple checkboxes for a has-many association.
+     *
+     * @param string             $name
+     * @param array              $collection
+     * @param array|\Traversable $checked Collection of checked values
+     * @param array              $labelAttributes
+     * @param bool               $returnAsArray
+     *
+     * @throws \InvalidArgumentException
+     * @return string|array
+     */
+    function form_collectionCheckBoxes($name, array $collection, $checked, array $labelAttributes = array(), $returnAsArray = false)
+    {
+        return nguyenanhung\Libraries\HTML\Form::collectionCheckBoxes($name, $collection, $checked, $labelAttributes, $returnAsArray);
+    }
+}
+if (!function_exists('form_radio')) {
+    /**
+     * Creates a radio button
+     *
+     *
+     *
+     * @param string $name
+     * @param string $value
+     * @param bool   $checked
+     * @param array  $attributes
+     *
+     * @return string
+     */
+    function form_radio($name, $value, $checked = false, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::radio($name, $value, $checked, $attributes);
+    }
+}
+if (!function_exists('form_collectionRadios')) {
+    /**
+     * Creates multiple radio buttons with labels
+     *
+     *
+     *
+     * @param string $name
+     * @param array  $collection
+     * @param mixed  $checked Checked value
+     * @param array  $labelAttributes
+     * @param bool   $returnAsArray
+     *
+     * @return array|string
+     */
+    function form_collectionRadios($name, array $collection, $checked, array $labelAttributes = array(), $returnAsArray = false)
+    {
+        return nguyenanhung\Libraries\HTML\Form::collectionRadios($name, $collection, $checked, $labelAttributes, $returnAsArray);
+    }
+}
+if (!function_exists('form_select')) {
+    /**
+     * Creates a select tag
+     * <code>
+     * // Simple select
+     * select('coffee_id', array('b' => 'black', 'w' => 'white'));
+     *
+     * // With option groups
+     * select('beverage', array(
+     *     'Coffee' => array('bc' => 'black', 'wc' => 'white'),
+     *     'Tea' => array('gt' => 'Green', 'bt' => 'Black'),
+     * ));
+     * </code>
+     *
+     * @param string $name       Name of the attribute
+     * @param array  $collection An associative array used for the option values
+     * @param mixed  $selected   Selected option Can be array or scalar
+     * @param array  $attributes HTML attributes
+     *
+     * @return string
+     */
+    function form_select($name, array $collection, $selected = null, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::select($name, $collection, $selected, $attributes);
+    }
+}
+if (!function_exists('form_option')) {
+    /**
+     * Creates an option tag
+     *
+     * @param string $value
+     * @param string $label
+     * @param array  $selected
+     *
+     * @return string
+     */
+    function form_option($value, $label, $selected)
+    {
+        return nguyenanhung\Libraries\HTML\Form::option($value, $label, $selected);
+    }
+}
+if (!function_exists('form_file')) {
+    /**
+     * Function form_file - Creates a file input field
+     *
+     * @param       $name
+     * @param array $attributes
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 15:31
+     */
+    function form_file($name, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::file($name, $attributes);
+    }
+}
+if (!function_exists('form_button')) {
+    /**
+     * Function form_button
+     *
+     * @param       $name
+     * @param       $text
+     * @param array $attributes
+     *
+     * @return string
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 15:22
+     */
+    function form_button($name, $text, array $attributes = array())
+    {
+        return nguyenanhung\Libraries\HTML\Form::button($name, $text, $attributes);
+    }
+}
+if (!function_exists('form_autoId')) {
+    /**
+     * Function form_autoId - Generate an ID given the name of an input
+     *
+     * @param $name
+     *
+     * @return string|null
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 15:13
+     */
+    function form_autoId($name)
+    {
+        return nguyenanhung\Libraries\HTML\Form::autoId($name);
     }
 }
