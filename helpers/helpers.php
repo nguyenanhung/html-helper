5d293432aabb08e539498932399829ca0023de18
nguyenanhung <info@nguyenanhung.com>
2023-01-31 17:14:34 +0700

    Add Highligh search keyword

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -583,3 +583,21 @@ if (!function_exists('form_autoId')) {
         return nguyenanhung\Libraries\HTML\Form::autoId($name);
     }
 }
+if (!function_exists('highlight_search_keyword')) {
+    /**
+     * Function highlight_search_keyword
+     *
+     * @param $keyword
+     * @param $string
+     * @param $font_color
+     *
+     * @return array|string|string[]
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 31/01/2023 13:58
+     */
+    function highlight_search_keyword($keyword, $string, $font_color = 'background:#d46220')
+    {
+        return nguyenanhung\Libraries\HTML\Highlight::highlightSearchKeyword($keyword, $string, $font_color);
+    }
+}
